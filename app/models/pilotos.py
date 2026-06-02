0929b4adf5a734b960d4acc3e75bfbe4327408ac
Fausto Leal <fausto.leal2099@gmail.com>
2026-06-02 11:16:13 -0300

    creando el pilotos model

---
+++ app/models/pilotos.py
@@ -0,0 +1,12 @@
+from utils.db import Base
+from sqlalchemy import Column, Integer
+from custom_types import NameType, NotNullableType
+
+
+class Pilotos(Base):
+  __tablename__ = "pilotos"
+
+  id = Column(Integer, primary_key=True, autoincrement=True)
+  name = Column(NameType, nullable=False)
+  usuario = Column(NotNullableType, nullable=False, unique=True)
+  passwordHash = Column(NotNullableType, nullable=False)
