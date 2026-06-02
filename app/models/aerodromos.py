17ea2f4d886d61a764a1b241c912d0786eecddda
Fausto Leal <fausto.leal2099@gmail.com>
2026-06-02 11:14:11 -0300

    eliminando el TypeDecorator del modelo e importandolo de custom_types

--- app/models/aerodromos.py
+++ app/models/aerodromos.py
@@ -1,33 +1,6 @@
 from utils.db import Base
 from sqlalchemy import Column
-from sqlalchemy.types import TypeDecorator, String
-
-
-class AerodromoType(TypeDecorator):
-  impl = String
-
-  def __init__(self,lenght=None, minLenght=1, *args, **kwargs):
-    super().__init__(lenght=lenght, *args, **kwargs)
-    self.minLenght = minLenght
-    self.maxLenght = lenght
-  
-  def process_bind_param(self, value, dialect):
-    if value is None:
-      raise ValueError("El Aerodromo no puede ser nulo")
-    
-    value = value.upper()
-
-    if not value.isalpha():
-      raise ValueError(f"El aerodromo {value} solo puede contener letras")
-
-    if len(value) < self.minLenght or len(value) > self.maxLenght:
-      raise ValueError(f"El aerodromo no puede tener mas de 4 letras o menos de 3")
-    
-    return value
-  
-
-  def process_result_value(self, value, dialect):
-    return value
+from custom_types import AerodromoType
 
 class Aerodromos(Base):
   __tablename__ = "aerodromos"
