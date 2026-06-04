4df15a54a485f342aee97640dcf78e11e5aa1dcf
Fausto Leal <fausto.leal2099@gmail.com>
2026-06-04 11:48:01 -0300

    Modificando el modelo Aviones

--- app/models/aviones.py
+++ app/models/aviones.py
@@ -5,7 +5,7 @@ from custom_types import MatriculaType, NotNullableType, ClaseType
 class Aviones(Base):
   __tablename__ = "aviones"
 
-  matricula = Column(MatriculaType, primary_key=True, unique=True, nullable=False)
-  modelo = Column(NotNullableType, nullable=False)
-  potencia = Column(NotNullableType, nullable=False)
-  clase = Column(ClaseType, nullable=False)
+  matricula = Column(MatriculaType(), primary_key=True, unique=True, nullable=False)
+  modelo = Column(NotNullableType(), nullable=False)
+  potencia = Column(NotNullableType(), nullable=False)
+  clase = Column(ClaseType(), nullable=False)
