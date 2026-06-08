dc8885c25acbcf97e9112fad7df7b0e1811ff66c
Fausto Leal <fausto.leal2099@gmail.com>
2026-06-08 11:24:40 -0300

    agregando ruta /aerodromos

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from utils.db import Base, engine
+from routers import aerodromos
+
+Base.metadata.create_all(engine)
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
   return "Hola mundo"
+
+app.include_router(aerodromos.router)
