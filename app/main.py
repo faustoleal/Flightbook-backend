0da3f0e50c64e551cfcf47bf9ff72ad249f97a6a
Fausto Leal <fausto.leal2099@gmail.com>
2026-06-08 11:48:38 -0300

    agregando la ruta /horas a el main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from utils.db import Base, engine
-from routers import aerodromos
+from routers import aerodromos, horas_de_vuelos
 
 Base.metadata.create_all(engine)
 
@@ -11,3 +11,4 @@ async def root():
   return "Hola mundo"
 
 app.include_router(aerodromos.router)
+app.include_router(horas_de_vuelos.router)
