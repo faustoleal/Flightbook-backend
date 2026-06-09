6da3022a4af7fbe000769ffd583b3ec1dbb57589
Fausto Leal <fausto.leal2099@gmail.com>
2026-06-09 12:49:45 -0300

    modificando la peticion get y creando la peticion post

--- app/routers/aerodromos.py
+++ app/routers/aerodromos.py
@@ -1,12 +1,33 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from utils.db import get_db
 from models.aerodromos import Aerodromos
+from schemas.aerodromos import AerodromoSchema
 
 router = APIRouter(
   prefix="/aerodromos"
 )
 
-@router.get("/")
+@router.get("/", response_model=list[AerodromoSchema])
 def getAerodromos(db:Session = Depends(get_db)):
-  return db.query(Aerodromos).all()
+  aerodromos= select(Aerodromos)
+  result = db.execute(aerodromos).scalars().all()
+  return result
+
+@router.post("/", response_model=AerodromoSchema)
+def createAerodromo(aerodromo:AerodromoSchema, db:Session = Depends(get_db)):
+  # 1. Verificar si ya existe (usando select + execute)
+    stmt = select(Aerodromos).where(Aerodromos.aerodromo == aerodromo.aerodromo)
+    existe = db.execute(stmt).scalars().first()
+    if existe:
+        raise HTTPException(status_code=400, message="El aeródromo ya existe")
+
+    # 2. Crear nuevo registro
+    nuevo = Aerodromos(**aerodromo.dict())
+    db.add(nuevo)
+    db.commit()
+    db.refresh(nuevo)
+
+    # 3. Devolver el objeto serializado con schema
+    return nuevo
