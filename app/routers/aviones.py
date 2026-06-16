e854880200cf62329547bd5de26d5f0749cfe6aa
Fausto Leal <fausto.leal2099@gmail.com>
2026-06-16 11:32:53 -0300

    modificando getAviones y creando post: createAvion

--- app/routers/aviones.py
+++ app/routers/aviones.py
@@ -1,10 +1,31 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from utils.db import get_db
 from models.aviones import Aviones
+from schemas.aviones import AvionSchema
 
 router = APIRouter(prefix="/aviones")
 
-@router.get("/")
+@router.get("/", response_model=list[AvionSchema])
 def getAviones(db:Session = Depends(get_db)):
-  return db.query(Aviones).all()
+  aviones = select(Aviones)
+  result = db.execute(aviones).scalars().all()
+  return result
+
+@router.post("/", response_model=AvionSchema)
+def createAvion(avion:AvionSchema, db:Session = Depends(get_db)):
+    # 1. Verificar si ya existe (usando select + execute)
+    stmt = select(Aviones).where(Aviones.matricula == avion.matricula)
+    existe = db.execute(stmt).scalars().first()
+    if existe:
+        raise HTTPException(status_code=400, message="El avión ya esta registrado")
+
+    # 2. Crear nuevo registro
+    nuevo = Aviones(**avion.model_dump())
+    db.add(nuevo)
+    db.commit()
+    db.refresh(nuevo)
+
+    # 3. Devolver el objeto serializado con schema
+    return nuevo
