80a56bd9e316502f56a661d6cdcc9637353ec2ab
Fausto Leal <fausto.leal2099@gmail.com>
2026-06-17 11:33:45 -0300

    modificando getPiltotos y creando createPiloto

--- app/routers/pilotos.py
+++ app/routers/pilotos.py
@@ -1,10 +1,31 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from utils.db import get_db
 from models.pilotos import Pilotos
+from schemas.pilotos import responsePilotoSchema, newPilotoSchema, PilotoSchema
 
 router = APIRouter(prefix="/pilotos")
 
-@router.get("/")
+@router.get("/", response_model=list[responsePilotoSchema])
 def getPilotos(db:Session = Depends(get_db)):
-  return db.query(Pilotos).all()
+  pilotos = select(Pilotos)
+  result = db.execute(pilotos).scalars().all()
+  return result
+
+@router.post("/", response_model=newPilotoSchema)
+def createPiloto(piloto:newPilotoSchema, db:Session = Depends(get_db)):
+# 1. Verificar si ya existe (usando select + execute)
+    stmt = select(Pilotos).where(Pilotos.usuario == piloto.usuario)
+    existe = db.execute(stmt).scalars().first()
+    if existe:
+        raise HTTPException(status_code=400, detail="El usuario ya esta registrado")
+
+    # 2. Crear nuevo registro
+    nuevo = Pilotos(**piloto.model_dump())
+    db.add(nuevo)
+    db.commit()
+    db.refresh(nuevo)
+
+    # 3. Devolver el objeto serializado con schema
+    return nuevo
