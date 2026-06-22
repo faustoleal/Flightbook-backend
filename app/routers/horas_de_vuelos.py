c0a179836f8428e32e3e30e010d0ad7a58d0b1b4
Fausto Leal <fausto.leal2099@gmail.com>
2026-06-22 13:15:55 -0300

    modificando peticion GET

--- app/routers/horas_de_vuelos.py
+++ app/routers/horas_de_vuelos.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, Depends
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from utils.db import get_db
 from models.horas_de_vuelo import HorasDeVuelo
+from schemas.horas_de_vuelos import HorasDeVueloResponse 
 
 router = APIRouter(prefix="/horas")
 
-@router.get("/")
+@router.get("/", response_model=list[HorasDeVueloResponse])
 def getHoras(db:Session = Depends(get_db)):
-  return db.query(HorasDeVuelo).all()
+  horas = select(HorasDeVuelo)
+  result = db.execute(horas).scalars().all()
+  return result
