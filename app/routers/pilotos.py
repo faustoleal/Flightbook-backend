bd25c7f9f8a71ae42892e851d2d839649a054d7d
Fausto Leal <fausto.leal2099@gmail.com>
2026-06-22 11:31:00 -0300

    creando route /:id y modificando detail de la route post

--- app/routers/pilotos.py
+++ app/routers/pilotos.py
@@ -13,13 +13,21 @@ def getPilotos(db:Session = Depends(get_db)):
   result = db.execute(pilotos).scalars().all()
   return result
 
+@router.get("/{id}", response_model=ResponsePilotoSchema)
+def getPilotoById(id:int, db:Session = Depends(get_db)):
+   piloto= select(Pilotos).where(Pilotos.id == id)
+   piloto_existe = db.execute(piloto).scalar_one_or_none()
+   if piloto_existe is None:
+      raise HTTPException(status_code=404, detail="El usuario no existe." )
+   return piloto_existe
+
 @router.post("/", response_model=NewPilotoSchema)
 def createPiloto(piloto:NewPilotoSchema, db:Session = Depends(get_db)):
 # 1. Verificar si ya existe (usando select + execute)
     stmt = select(Pilotos).where(Pilotos.usuario == piloto.usuario)
     existe = db.execute(stmt).scalars().first()
     if existe:
-        raise HTTPException(status_code=400, detail="El usuario ya esta registrado")
+        raise HTTPException(status_code=400, detail="El usuario ya esta registrado.")
 
     # 2. Crear nuevo registro
     nuevo = Pilotos(**piloto.model_dump())
