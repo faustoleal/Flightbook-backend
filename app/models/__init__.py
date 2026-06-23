833ce46125fde2943dfa570573c5de1c29c9c558
Fausto Leal <fausto.leal2099@gmail.com>
2026-06-23 12:43:10 -0300

    creando archivo __init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,11 @@
+from .aerodromos import Aerodromos
+from .aviones import Aviones
+from .horas_de_vuelo import HorasDeVuelo
+from .pilotos import Pilotos
+
+__all__ = [
+  "Aerodromos",
+  "Aviones",
+  "HorasDeVuelo",
+  "Pilotos"
+]
