06f580e1897a863f9a16a4a7e3a595cd269873b7
Fausto Leal <fausto.leal2099@gmail.com>
2026-06-23 12:48:09 -0300

    creando route /:id?page=int

--- app/routers/horas_de_vuelos.py
+++ app/routers/horas_de_vuelos.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
-from sqlalchemy.orm import Session
+from sqlalchemy import func, select
+from sqlalchemy.orm import Session, joinedload
 from utils.db import get_db
-from models.horas_de_vuelo import HorasDeVuelo
-from schemas.horas_de_vuelos import HorasDeVueloResponse 
+from models import HorasDeVuelo
+from schemas import HorasDeVueloResponse, PaginationHorasResponse
 
 router = APIRouter(prefix="/horas")
 
@@ -12,3 +12,29 @@ def getHoras(db:Session = Depends(get_db)):
   horas = select(HorasDeVuelo)
   result = db.execute(horas).scalars().all()
   return result
+
+@router.get("/{id}", response_model=PaginationHorasResponse)
+def getHorasPorPiloto(id:int, page:int = 1, db:Session = Depends(get_db)):
+  limit = 15
+  offset = (page - 1) * limit if page > 0 else 0
+
+  smt = (
+    select(HorasDeVuelo)
+    .where(HorasDeVuelo.piloto_id == id)
+    .order_by(HorasDeVuelo.dia.asc())
+    .options(
+       joinedload(HorasDeVuelo.avion),
+       joinedload(HorasDeVuelo.piloto)
+    )
+    .limit(limit)
+    .offset(offset)
+    )
+  
+  result = db.execute(smt).scalars().all()
+  total = db.execute(
+    select(func.count(HorasDeVuelo.id)).where(HorasDeVuelo.piloto_id == id)).scalar()
+
+  return {
+    "content": result,
+    "totalPages": (total + limit -1) // limit
+  }
