524b912035e04d385ca8cbd504a5b8c2d31aeecb
Fausto Leal <fausto.leal2099@gmail.com>
2026-06-24 12:06:55 -0300

    agregando el middleware TokenExtractor y creando una funcion para probarlo

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from utils.db import Base, engine
+from utils.middelwares import TokenExtractor
 from routers import aerodromos, horas_de_vuelos, aviones, pilotos
+
 
 Base.metadata.create_all(engine)
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-  return "Hola mundo"
+async def root(request: Request):
+  return {"token": request.state.token}
+
+app.add_middleware(TokenExtractor)
 
 app.include_router(aerodromos.router)
 app.include_router(horas_de_vuelos.router)
