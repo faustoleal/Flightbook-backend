5caf22cb3f2f560c1618e3a9581f2b5914ffa266
Fausto Leal <fausto.leal2099@gmail.com>
2026-07-01 11:33:31 -0300

    importando bcrypt para hashear constraseñas en el POST

--- app/routers/pilotos.py
+++ app/routers/pilotos.py
@@ -3,9 +3,11 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 from utils.db import get_db
 from models import Pilotos
+import bcrypt
 from schemas import ResponsePilotoSchema, NewPilotoSchema
 
 router = APIRouter(prefix="/pilotos")
+
 
 @router.get("/", response_model=list[ResponsePilotoSchema])
 def getPilotos(db:Session = Depends(get_db)):
@@ -23,17 +25,32 @@ def getPilotoById(id:int, db:Session = Depends(get_db)):
 
 @router.post("/", response_model=NewPilotoSchema)
 def createPiloto(piloto:NewPilotoSchema, db:Session = Depends(get_db)):
-# 1. Verificar si ya existe (usando select + execute)
+    # 1. Verificar si ya existe y si el formulario esta completo
+    if not piloto.name or not piloto.usuario or not piloto.password:
+       raise HTTPException(status_code=401, detail="El formulario no esta completo.")
+    
     stmt = select(Pilotos).where(Pilotos.usuario == piloto.usuario)
     existe = db.execute(stmt).scalars().first()
     if existe:
         raise HTTPException(status_code=400, detail="El usuario ya esta registrado.")
+    
+    # Hash de contraseña
+
+    salt = bcrypt.gensalt(rounds=10)
+    password_hash = bcrypt.hashpw(piloto.password.encode("utf-8")[:72],salt).decode("utf-8")
+
+    # Crear piloto
+
+    new_piloto = Pilotos(
+       name=piloto.name,
+       usuario = piloto.usuario,
+       password_hash = password_hash
+    )
 
     # 2. Crear nuevo registro
-    nuevo = Pilotos(**piloto.model_dump())
-    db.add(nuevo)
+    db.add(new_piloto)
     db.commit()
-    db.refresh(nuevo)
+    db.refresh(new_piloto)
 
     # 3. Devolver el objeto serializado con schema
-    return nuevo
+    return new_piloto
