24f5c4485a3657006684ca1fab84edc08f1b2476
Fausto Leal <fausto.leal2099@gmail.com>
2026-07-01 12:23:11 -0300

    agregando el router de login

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from utils.db import Base, engine
 from utils.middlewares import TokenExtractor
-from routers import aerodromos, horas_de_vuelos, aviones, pilotos
+from routers import aerodromos, horas_de_vuelos, aviones, pilotos, login
 
 
 Base.metadata.create_all(engine)
@@ -18,3 +18,4 @@ app.include_router(aerodromos.router)
 app.include_router(horas_de_vuelos.router)
 app.include_router(aviones.router)
 app.include_router(pilotos.router)
+app.include_router(login.router)
