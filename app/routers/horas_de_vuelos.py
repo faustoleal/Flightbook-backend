33302f77fa87ba3bcaf9eb95a1f1badd979f766a
Fausto Leal <fausto.leal2099@gmail.com>
2026-07-07 11:34:59 -0300

    agregando ruta /{id}/totales

--- app/routers/horas_de_vuelos.py
+++ app/routers/horas_de_vuelos.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import func, select
 from sqlalchemy.orm import Session, joinedload
 from utils.db import get_db
 from models import HorasDeVuelo
-from schemas import HorasDeVueloResponse, PaginationHorasResponse
+from schemas import HorasDeVueloResponse, PaginationHorasResponse, HorasDeVuelosTotalesResponse
 
 router = APIRouter(prefix="/horas")
 
@@ -38,3 +38,40 @@ def getHorasPorPiloto(id:int, page:int = 1, db:Session = Depends(get_db)):
     "content": result,
     "totalPages": (total + limit -1) // limit
   }
+
+@router.get("/{id}/totales", response_model=HorasDeVuelosTotalesResponse)
+async def getTotales(id:int, db:Session = Depends(get_db)):
+    query = select(
+            func.sum(
+                HorasDeVuelo.local_dia_p + HorasDeVuelo.local_dia_c +
+                HorasDeVuelo.travesia_dia_p + HorasDeVuelo.travesia_dia_c
+            ).label("total_dia"),
+            func.sum(
+                HorasDeVuelo.local_noche_p + HorasDeVuelo.local_noche_c +
+                HorasDeVuelo.travesia_noche_p + HorasDeVuelo.travesia_noche_c
+            ).label("total_noche"),
+            func.sum(
+                HorasDeVuelo.local_dia_p + HorasDeVuelo.local_noche_p +
+                HorasDeVuelo.local_noche_p + HorasDeVuelo.local_noche_c
+            ).label("total_local"),
+            func.sum(HorasDeVuelo.travesia_dia_p).label("total_travesia"),
+            func.sum(
+                HorasDeVuelo.local_dia_p + HorasDeVuelo.local_noche_p +
+                HorasDeVuelo.travesia_dia_p + HorasDeVuelo.travesia_noche_p
+            ).label("total_alMando"),
+            func.sum(
+                HorasDeVuelo.local_dia_c + HorasDeVuelo.local_noche_c +
+                HorasDeVuelo.travesia_dia_c + HorasDeVuelo.travesia_noche_c +
+                HorasDeVuelo.local_dia_p + HorasDeVuelo.local_noche_p +
+                HorasDeVuelo.travesia_dia_p + HorasDeVuelo.travesia_noche_p
+            ).label("total_horas"),
+            func.sum(HorasDeVuelo.aterrizajes).label("total_aterrizajes"),
+        ).where(HorasDeVuelo.piloto_id == id)
+    
+    result = db.execute(query)
+    totales = result.mappings().first()
+
+    if not totales:
+      return HTTPException(status_code=404, detail="No hay horas registradas")
+    
+    return totales
